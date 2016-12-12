c5dcdb7a00a97e73a02f8e4ae648325bee8d9834
Mbusi Hlatshwayo <hlatshwayo@cerebro.cs.xu.edu>
2016-12-12 12:54:30 -0500

    clean up test

--- PhaseThree/initProc.c
+++ PhaseThree/initProc.c
@@ -37,53 +37,9 @@ void test()
 	state_t processState;
 	segTbl_t* segTbl;
 	
-	/*Init ksegos*/
-	/*KSegOS.header = Something or other*/ /*Question: Magic num (42 in hex)  | 64*/
-	for(int i=0; i < KUSEGOSSIZE  ;i++){
-		KSegOS.pteTable[i].pte_entryHI = (0X2000 + i) << 12;
-		KSegOS.pteTable[i].pte_entryLO = ((0X2000 + i) << 12)| DIRTYON | GLOBALON | VALIDON;
-		
-	 }
-	
-	/*initialize swap pool */
-	for(int i = 0; i < SWAPPAGES; i++){ 
-		swapTables[i].sw_asid = -1; 
-		swapTables[i].sw_pte = NULL;
-	}
-	
 	/*Init sema4 array*/
 	for (int i = 0; i < (DEVICELISTNUM*DEVICENUM); i++) {
 		sema4array[i] = 1; 
-	}
-	
-	/* big loop for init processes */
-	for(int i = 1; i < MAXPAGEPROCS; i++) {
-		procs[i-1].Tp_pte.header = 1;
-		
-		for(int j = 0; j < KUSEGPTESIZE; j++)
-		{
-			procs[i-1].Tp_pte.pteTable[j].pte_entryHI = (0x8000 + j) << 12; /*Question: shifted correctly?*/
-			procs[i-1].Tp_pte.pteTable[j].pte_entryLO = ALLOFF | DIRTYON;
-		}
-		
-		/*Las entry in the table has different entry hi*/
-		procs[i-1].Tp_pte.pteTable[KUSEGPTESIZE-1].pte_entryHI = 0xBFFFF << 12; /*Question: shifted correctly?*/
-		
-		/*seg table init*/
-		segTbl = (segTbl_t *) STARTADDR; /*Question: What is segment table width? 3 ptrs for each ASID (64 x 3)*/
-		segTbl->ksegOS = &KSegOS;
-		segTbl->kUseg2 = &(procs[i-1].Tp_pte);
-		
-		/*set up the state for process*/
-		processState.s_pc = (memaddr) midwife;
-		processState.s_t9 = (memaddr) midwife;
-		processState.s_status = ALLOFF | IEc | TE | IM;
-		processState.s_asid = i << 6;
-		processState.s_sp = 0; /*Question: Math- 0x200000 + 64 or something...*/
-		
-
-		
-		SYSCALL(CREATEPROCESS,(int)&processState,0,0);
 	}
 	
 	/*P on the device sema4s*/
@@ -92,6 +48,49 @@ void test()
 		SYSCALL(PASSEREN, (int)&masterSem,0,0);
 	}
 	
+	/*initialize swap pool */
+	for(int i = 0; i < SWAPPAGES; i++){ 
+		swapTables[i].sw_asid = -1; 
+		swapTables[i].sw_pte = NULL;
+	}
+	
+	/*Init ksegos*/
+	KSegOS.header = (MAGICNUMBER << 24) | KUSEGOSSIZE;
+	for(int i=0; i < KUSEGOSSIZE  ;i++){
+		KSegOS.pteTable[i].pte_entryHI = (SEGOSADDR + i) << 12;
+		KSegOS.pteTable[i].pte_entryLO = ((SEGOSADDR + i) << 12)| DIRTYON | GLOBALON | VALIDON;
+		
+	 }
+	
+	/* big loop for init processes */
+	for(int i = 1; i < MAXPAGEPROCS +1; i++) {
+		procs[i-1].Tp_pte.header = 1;
+		
+		for(int j = 0; j < KUSEGPTESIZE; j++)
+		{
+			procs[i-1].Tp_pte.pteTable[j].pte_entryHI = (SEGTWOADDR + j) << 12; 
+			procs[i-1].Tp_pte.pteTable[j].pte_entryLO = ALLOFF | DIRTYON;
+		}
+		
+		/*Last entry in the table has different entry hi*/
+		procs[i-1].Tp_pte.pteTable[KUSEGPTESIZE].pte_entryHI = LASTADDR << 12;
+		
+		/*seg table init*/
+		segTbl = (segTbl_t *) STARTADDR + (i * 24); /*multiply by segtable width*/
+		segTbl->ksegOS = &KSegOS;
+		segTbl->kUseg2 = &(procs[i].Tp_pte);
+		
+		/*set up the state for process*/
+		processState.s_pc = (memaddr) midwife;
+		processState.s_t9 = (memaddr) midwife;
+		processState.s_status = ALLOFF | IEc | TE | IM;
+		processState.s_asid = i << 6;
+		processState.s_sp = ROMPAGESTART + 64; /*Question: what....*/
+		
+
+		
+		SYSCALL(CREATEPROCESS,(int)&processState,0,0);
+	}
 	
 	/* call sys 2 the end! */
 	SYSCALL(TERMINATEPROCESS, 0,0,0);
@@ -99,6 +98,7 @@ void test()
 
 
 void midwife(){
+	
 	
 	/*header for kuseg2 is magic num(42) | 31*/
 

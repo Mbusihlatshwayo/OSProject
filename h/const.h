b6e4271be6c2a2fd659fb406853f0da0d29e5d88
Mbusi Hlatshwayo <hlatshwayo@cerebro.cs.xu.edu>
2016-12-17 21:41:22 -0500

    Format Fixes

--- h/const.h
+++ h/const.h
@@ -19,55 +19,55 @@
 #define ROMPAGESTART	0x20000000	 /* ROM Reserved Page */
 
 /*Clock values*/
-#define INTERVALTIME 100000000 /*time put into PsuedoClock (interval)*/
-#define CPUADDTIME 100000 /*time for scheduler (CPU specific)*/
-#define TIMESLICE 5000 	 /*time set for timers*/
+#define INTERVALTIME 	100000000 /*time put into PsuedoClock (interval)*/
+#define CPUADDTIME 		100000 /*time for scheduler (CPU specific)*/
+#define TIMESLICE 		5000 	 /*time set for timers*/
 
 /* SYSCALL values*/
-#define CREATEPROCESS 1
-#define TERMINATEPROCESS 2
-#define VERHOGEN 3
-#define PASSEREN 4
-#define SPECTRAPVEC 5
-#define GETCPUTIME 6
-#define WAITCLOCK 7 
-#define WAITIO 8
+#define CREATEPROCESS 		1
+#define TERMINATEPROCESS 	2
+#define VERHOGEN 			3
+#define PASSEREN 			4
+#define SPECTRAPVEC 		5
+#define GETCPUTIME 			6
+#define WAITCLOCK 			7 
+#define WAITIO 				8
 
-#define WRITETERMINAL 10
-#define WRITEPRINTER 16
-#define GETTOD 17
-#define TERMINATE 18
+#define WRITETERMINAL 		10
+#define WRITEPRINTER 		16
+#define GETTOD 				17
+#define TERMINATE 			18
 
 
 /* timer, timescale, TOD-LO and other bus regs */
-#define RAMBASEADDR	0x10000000
-#define TODLOADDR	0x1000001C
-#define INTERVALTMR	0x10000020	
+#define RAMBASEADDR		0x10000000
+#define TODLOADDR		0x1000001C
+#define INTERVALTMR		0x10000020	
 #define TIMESCALEADDR	0x10000024
 
 /* reserved areas*/
-#define ADDRESS1 0x10000000
-#define NEWSYSCALL 0x200003D4
-#define NEWTRAP 0x200002BC
-#define NEWTLB 0x200001A4
-#define NEWINTERRUPT 0x2000008C
-#define OLDSYSCALL 0x20000348
-#define OLDTRAP 0x20000230
-#define OLDTLB 	0x200001A4
-#define OLDINTERRUPT 0x20000000
+#define ADDRESS1 		0x10000000
+#define NEWSYSCALL 		0x200003D4
+#define NEWTRAP 		0x200002BC
+#define NEWTLB 			0x200001A4
+#define NEWINTERRUPT 	0x2000008C
+#define OLDSYSCALL 		0x20000348
+#define OLDTRAP 		0x20000230
+#define OLDTLB 			0x200001A4
+#define OLDINTERRUPT 	0x20000000
 
 /*semaphore number for devices*/
-#define DEVICELISTNUM 6
-#define DEVICENUM 8
+#define DEVICELISTNUM 	6
+#define DEVICENUM 		8
 
 
 /* utility constants */
 #define	TRUE		1
 #define	FALSE		0
-#define ON              1
-#define OFF             0
+#define ON          1
+#define OFF         0
 #define HIDDEN		static
-#define EOS		'\0'
+#define EOS			'\0'
 
 #define NULL ((void *)0xFFFFFFFF)
 
@@ -78,7 +78,6 @@
 #define TLBTRAP		0
 #define PROGTRAP	1
 #define SYSTRAP		2
-
 #define TRAPTYPES	3
 
 /* device interrupts */
@@ -129,24 +128,24 @@
 #define LDIT(T)	((* ((cpu_t *) INTERVALTMR)) = (T) * (* ((cpu_t *) TIMESCALEADDR))) 
 
 /* hexadecimal values for IE, KU, BEV, and VM (for Status)*/
-#define ALLOFF 0x00000000	/* everything is off bit */
-#define IEc 0x00000001		/* global interrupt enable bit */
-#define KUc 0x00000002		/* The “current” kernel-mode user-mode control bit.*/
+#define ALLOFF 	0x00000000		/* everything is off bit */
+#define IEc 	0x00000001		/* global interrupt enable bit */
+#define KUc 	0x00000002		/* The “current” kernel-mode user-mode control bit.*/
 
-#define IEp 0x00000004		/* the “previous” settings of the Status.IEc */
-#define KUp 0x00000008		/* the “previous” settings of the Status.KUc*/
-#define IEo 0x00000010		/* the “previous” settings of the Status.IEp*/
-#define KUo 0x00000020		/* the “previous” settings of the Status.KUp*/
+#define IEp 	0x00000004		/* the “previous” settings of the Status.IEc */
+#define KUp 	0x00000008		/* the “previous” settings of the Status.KUc*/
+#define IEo 	0x00000010		/* the “previous” settings of the Status.IEp*/
+#define KUo 	0x00000020		/* the “previous” settings of the Status.KUp*/
 
-#define IM  0x0000ff00		/* Interrupt Mask. An 8-bit mask that enables/disables external interrupts.*/
+#define IM  	0x0000ff00		/* Interrupt Mask. An 8-bit mask that enables/disables external interrupts.*/
 
-#define BEV 0x00400000		/* The Bootstrap Exception Vector. This bit determines the starting address for the exception vectors.*/
+#define BEV 	0x00400000		/* The Bootstrap Exception Vector. This bit determines the starting address for the exception vectors.*/
 
-#define VMc 0x01000000		/* The “current” VM on/off flag bit. Status.VMc=0 indicates that virtual memory translation is currently off.*/
-#define VMp 0x02000000		/* the “previous” setting of the Status.VMc bit*/
-#define VMo 0x04000000		/* the “previous” setting of the Status.VMp bit*/
+#define VMc 	0x01000000		/* The “current” VM on/off flag bit. Status.VMc=0 indicates that virtual memory translation is currently off.*/
+#define VMp 	0x02000000		/* the “previous” setting of the Status.VMc bit*/
+#define VMo 	0x04000000		/* the “previous” setting of the Status.VMp bit*/
 
-#define TE  0x08000000		/* the processor Local Timer enable bit. A 1-bit mask that enables disables the processor’s Local Timer.*/
+#define TE  	0x08000000		/* the processor Local Timer enable bit. A 1-bit mask that enables disables the processor’s Local Timer.*/
 
 /*hex values for line addresses for cause bits*/
 #define LINEADDR_0 0x00000100
@@ -176,23 +175,23 @@
 #define WORD_4 0x1000004C
 
 /*Phase 3 constants*/
-#define MAXPAGEPROCS 8
-#define SWAPPAGES 24
-#define SEGWIDTH 12
-#define KUSEGPTESIZE 32
-#define KUSEGOSSIZE 64
-#define MAGICNUMBER 0x2A /*42*/
-#define DISKCOUNT 8
-#define TAPECOUNT 8
-#define HEADLOC   0
-#define TLBL 	  2
-#define TLBS	  3
+#define MAXPAGEPROCS 	8
+#define SWAPPAGES 		24
+#define SEGWIDTH 		12
+#define KUSEGPTESIZE 	32
+#define KUSEGOSSIZE 	64
+#define MAGICNUMBER 	0x2A /*42*/
+#define DISKCOUNT 		8
+#define TAPECOUNT 		8
+#define HEADLOC   		0
+#define TLBL 	  		2
+#define TLBS	  		3
 
-#define OSEND (PAGESIZE * KUSEGOSSIZE)
-#define KSEGEND (ROMPAGESTART + OSEND)
-#define BUFFERSTART (KSEGEND - (DISKCOUNT * PAGESIZE))
-#define TAPEBUFFSTART (OSEND - ((TAPECOUNT * PAGESIZE) + (DISKCOUNT* PAGESIZE))
-#define SYSSTACK (PAGESIZE * SYSTRAP)
+#define OSEND 			(PAGESIZE * KUSEGOSSIZE)
+#define KSEGEND 		(ROMPAGESTART + OSEND)
+#define BUFFERSTART 	(KSEGEND - (DISKCOUNT * PAGESIZE))
+#define TAPEBUFFSTART 	(OSEND - ((TAPECOUNT * PAGESIZE) + (DISKCOUNT* PAGESIZE))
+#define SYSSTACK 		(PAGESIZE * SYSTRAP)
 
 #define TAPEDEV (((TAPEINT-3) * DEVREGSIZE * DEVPERINT) + DEVICEREGS)
 #define DISKDEV (((DISKINT-3) * DEVREGSIZE * DEVPERINT) + DEVICEREGS)
@@ -200,25 +199,25 @@
 #define PRNTDEV (((PRNTINT-3) * DEVREGSIZE * DEVPERINT) + DEVICEREGS)
 
 /*Phase 3 Addresses*/
-#define STARTADDR 0x20000500
-#define SEGOSADDR 0x2000
-#define SEGTWOADDR 0x8000
-#define LASTADDR 0xBFFFF
-#define PAGE52ADDR 0x8000000B
-#define LASTSEG2PG 0xC0000000
-#define DEVICEREGS 0x10000050
+#define STARTADDR 	0x20000500
+#define SEGOSADDR 	0x2000
+#define SEGTWOADDR 	0x8000
+#define LASTADDR 	0xBFFFF
+#define PAGE52ADDR 	0x8000000B
+#define LASTSEG2PG 	0xC0000000
+#define DEVICEREGS 	0x10000050
 
 /*Bits for Phase 3*/
-#define DIRTYON 0x00000400
-#define GLOBALON 0x00000100
-#define VALIDON 0x00000200
-#define MASKBIT 0x00000FC0
+#define DIRTYON 	0x00000400
+#define GLOBALON 	0x00000100
+#define VALIDON 	0x00000200
+#define MASKBIT 	0x00000FC0
 
 /*YellowBook page 40 const*/
-#define EOT 0
-#define EOF 1
-#define EOB 2
-#define TS 3
+#define EOT 	0
+#define EOF 	1
+#define EOB 	2
+#define TS 		3
 
 
 
